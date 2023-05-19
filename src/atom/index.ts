0c9ae94211a2b2466ee9866593f6622dc9b88b38
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-05-19 10:26:04 +0900

    👔 add: props toggle func

--- src/atom/index.ts
+++ src/atom/index.ts
@@ -3,3 +3,18 @@ import { CardType } from '@/common/type'
 import { atom } from 'jotai'
 
 export const cardListAtom = atom<CardType[]>(cardListData)
+export const toggleCardStatusAtom = atom(
+  (get) => get(cardListAtom),
+  (_, set, update: Omit<CardType, 'status'>) => {
+    set(cardListAtom, (prev) => {
+      prev.map((card) => {
+        if (card.id === update.id && card.mark === update.mark) {
+          card.status = card.status === 'open' ? 'close' : 'open'
+        }
+        return card
+      })
+      const newCardList = [...prev]
+      return newCardList
+    })
+  }
+)
--- src/component/card/BackCard.tsx
+++ src/component/card/BackCard.tsx
@@ -1,14 +1,19 @@
 'use client'
 
+import { toggleCardStatusAtom } from '@/atom'
 import { CardType } from '@/common/type'
+import { useSetAtom } from 'jotai'
 import { NextPage } from 'next'
 
 /**
  * @package
  */
 
-export const BackCard: NextPage<Pick<CardType, 'id'>> = ({ id }) => {
-  const handleToggle = () => {}
+export const BackCard: NextPage<Omit<CardType, 'status'>> = ({ id, mark }) => {
+  const toggleCardStatus = useSetAtom(toggleCardStatusAtom)
+  const handleToggle = () => {
+    toggleCardStatus({ id, mark })
+  }
   return (
     <div
       className='hover: col-span-1 h-36 w-20 rounded-lg bg-white p-2 outline-yellow-300 hover:cursor-pointer hover:outline hover:outline-4 hover:outline-offset-4'
