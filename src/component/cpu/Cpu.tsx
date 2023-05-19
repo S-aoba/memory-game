97e5b29593b3f5be607c98332bea3c83fc1eb48c
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-05-19 21:37:09 +0900

    💄 fix:UI

--- src/component/cpu/Cpu.tsx
+++ src/component/cpu/Cpu.tsx
@@ -7,7 +7,7 @@ export const Cpu = () => {
   return (
     <div className=' h-full max-h-[800px] w-11/12 max-w-[600px] border border-solid'>
       <div className=' flex h-1/4 w-full items-center justify-center'>
-        <p className=' text-5xl'>Your</p>
+        <p className=' text-5xl'>CPU</p>
       </div>
       <div className=" h-3/4 bg-blue-500">
         <span>Hello</span>
