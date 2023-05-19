be40cbb20a4860c88470312229b18c649549df7a
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-05-19 09:33:52 +0900

    ♻️ refactor: nest component

---
+++ src/common/card-data.ts
@@ -0,0 +1,46 @@
+import { CardType } from './type'
+
+export const cardListData: CardType[] = [
+  {
+    id: 1,
+    mark: 'hart',
+  },
+  {
+    id: 2,
+    mark: 'hart',
+  },
+  {
+    id: 3,
+    mark: 'hart',
+  },
+  {
+    id: 4,
+    mark: 'hart',
+  },
+
+  {
+    id: 5,
+    mark: 'hart',
+  },
+  {
+    id: 1,
+    mark: 'spade',
+  },
+  {
+    id: 2,
+    mark: 'spade',
+  },
+  {
+    id: 3,
+    mark: 'spade',
+  },
+  {
+    id: 4,
+    mark: 'spade',
+  },
+
+  {
+    id: 5,
+    mark: 'spade',
+  },
+]
