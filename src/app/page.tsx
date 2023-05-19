d86a57f5baa315a51581a14f7d6af4adacc66b2d
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-05-19 09:31:14 +0900

    🩹 fix:UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function GameBoard() {
   return (
     <main className='flex h-screen w-screen items-center justify-center bg-gray-400'>
       <div className=' flex h-full max-h-[800px] w-11/12 max-w-[800px] items-center justify-center rounded-md bg-green-600 px-3 py-5'>
-        <div className=' grid h-full w-full grid-cols-7 place-content-between place-items-center'>
+        <div className=' grid h-full w-full grid-cols-7 place-content-around place-items-center'>
           {cardList.map((card, index) => {
             return <Card key={index} id={card.id} mark={card.mark} />
           })}
