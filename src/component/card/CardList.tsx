82a3248d94aac0c3f0702fdb6b1b8b748c96715d
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-05-21 16:30:18 +0900

    🩹 fix:display

--- src/component/card/CardList.tsx
+++ src/component/card/CardList.tsx
@@ -3,14 +3,22 @@
 import { useAtomValue } from 'jotai'
 import { Card } from './Card'
 import { cardListAtom } from '@/atom/cardAtom'
+import { boardAtom } from '@/atom/tableAtom'
 
 export const CardList = () => {
+  const board = useAtomValue(boardAtom)
   const cardList = useAtomValue(cardListAtom)
   return (
-    <div className=' grid h-full w-full grid-cols-7 place-content-around place-items-center'>
-      {cardList.map((card, index) => {
-        return <Card key={index} id={card.id} mark={card.mark} status={card.status} />
-      })}
-    </div>
+    <>
+      {board.isGameStart ? (
+        <div className=' grid h-full w-full grid-cols-7 place-content-around place-items-center'>
+          {cardList.map((card, index) => {
+            return <Card key={index} id={card.id} mark={card.mark} status={card.status} />
+          })}
+        </div>
+      ) : (
+        <p>まだ試合は始まっていません</p>
+      )}
+    </>
   )
 }
