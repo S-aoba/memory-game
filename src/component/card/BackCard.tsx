cfcf7a892d2fc293c533a487ac486d0b1737b8b7
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-05-24 22:16:20 +0900

    🩹 fix:css

--- src/component/card/BackCard.tsx
+++ src/component/card/BackCard.tsx
@@ -15,13 +15,13 @@ export const BackCard: NextPage<CardType> = ({ id, mark, status }) => {
     <>
       {status !== null ? (
         <div
-          className='col-span-1 h-36 w-20 rounded-lg bg-white p-2 outline-yellow-300 hover:cursor-pointer hover:outline hover:outline-4 hover:outline-offset-4'
+          className='col-span-1 h-36 w-20 rounded-lg bg-white p-2 hover:outline-yellow-300 hover:cursor-pointer hover:outline hover:outline-4 hover:outline-offset-4'
           onClick={() => handleUserTurn({ id, mark, status })}
         >
           <div className='h-full w-full rounded-lg bg-slate-500'></div>
         </div>
       ) : (
-        <div className='invisible col-span-1 h-36 w-20 rounded-lg bg-white p-2 outline-yellow-300 hover:cursor-pointer hover:outline hover:outline-4 hover:outline-offset-4'>
+        <div className='invisible col-span-1 h-36 w-20 rounded-lg bg-white p-2 hover:outline-yellow-300 hover:cursor-pointer hover:outline hover:outline-4 hover:outline-offset-4'>
           <div className='h-full w-full rounded-lg bg-slate-500'></div>
         </div>
       )}
