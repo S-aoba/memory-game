360d147398560e07efb6f9ea79333697f3b2fafa
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-05-25 13:34:45 +0900

    🚚 rename:func

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import Link from 'next/link'
 import { startGameAtom } from '@/atom/boardAtom'
 
 export default function StartPage() {
-  const startGame = useSetAtom(startGameAtom)
+  const handleStartGame = useSetAtom(startGameAtom)
 
   return (
     <div className=' flex h-screen w-screen items-center justify-center'>
       <Link href={'/board'}>
-        <button onClick={startGame} className=' rounded-lg bg-blue-500 px-4 py-2 text-white'>
+        <button onClick={handleStartGame} className=' rounded-lg bg-blue-500 px-4 py-2 text-white'>
           GameStart
         </button>
       </Link>
