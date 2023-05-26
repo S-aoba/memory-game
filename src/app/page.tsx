b908972ad3eab9d7de900daad013ebd680f49690
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-05-26 14:44:13 +0900

    💄 update:start UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,10 +9,22 @@ export default function StartPage() {
   const handleStartGame = useSetAtom(startGameAtom)
 
   return (
-    <div className=' flex h-screen w-screen items-center justify-center'>
+    <div className=' flex h-screen w-screen items-center justify-center gap-x-10'>
       <Link href={'/board'}>
         <button onClick={handleStartGame} className=' rounded-lg bg-blue-500 px-4 py-2 text-white'>
-          GameStart
+          Easy
+        </button>
+      </Link>
+
+      <Link href={'/board'}>
+        <button onClick={handleStartGame} className=' rounded-lg bg-amber-500 px-4 py-2 text-white'>
+          Normal
+        </button>
+      </Link>
+
+      <Link href={'/board'}>
+        <button onClick={handleStartGame} className=' rounded-lg bg-red-500 px-4 py-2 text-white'>
+          Hard
         </button>
       </Link>
     </div>
