4f2e536c9815903d5f2c81eadecfd40542f47033
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-05-30 10:17:47 +0900

    👔 add: normal mode secondUserTurn logic

--- src/common/hook/useNormalMode.tsx
+++ src/common/hook/useNormalMode.tsx
@@ -1,15 +1,24 @@
-import { useSetAtom } from 'jotai'
+import { useAtomValue, useSetAtom } from 'jotai'
 
-import { setCardStatusToOpenAtom } from '@/atom/boardAtom'
-import { addSelectedCardAtom } from '@/atom/userAtom'
+import { setCardStatusToCloseAtom, setCardStatusToNullAtom, setCardStatusToOpenAtom } from '@/atom/boardAtom'
+import { addSelectedCardAtom, addUserCardListAtom, resetSelectedCardAto, userAtom } from '@/atom/userAtom'
 
 import type { CardType } from '../type'
 import { useAudio } from './useAudio'
+import { useTimer } from './useTimer'
 
 export const useNormalMode = () => {
+  const selectedCard = useAtomValue(userAtom).selectedCard
+
+  const resetSelectedCard = useSetAtom(resetSelectedCardAto)
   const addSelectedCard = useSetAtom(addSelectedCardAtom)
+  const addUserCardList = useSetAtom(addUserCardListAtom)
 
   const setCardStatusToOpen = useSetAtom(setCardStatusToOpenAtom)
+  const setCardStatusToNull = useSetAtom(setCardStatusToNullAtom)
+  const setCardStatusToClose = useSetAtom(setCardStatusToCloseAtom)
+
+  const { waitSeconds } = useTimer()
 
   const { flipAudio } = useAudio()
 
@@ -18,8 +27,27 @@ export const useNormalMode = () => {
     setCardStatusToOpen(firstCard)
     addSelectedCard(firstCard)
   }
-  const secondUserTurnNormalMode = (secondCard: CardType) => {
+
+  const secondUserTurnNormalMode = async (secondCard: CardType) => {
+    flipAudio.play()
+    setCardStatusToOpen(secondCard)
+    if (!selectedCard) return
+    const isPair: boolean = selectedCard.id === secondCard.id && selectedCard.mark !== secondCard.mark
+
+    if (isPair) {
+      await waitSeconds(1200)
+      setCardStatusToNull(selectedCard, secondCard)
+      addUserCardList(selectedCard, secondCard)
+      resetSelectedCard()
+      return
+    }
+
+    await waitSeconds(1200)
+    setCardStatusToClose(selectedCard, secondCard)
+    resetSelectedCard()
     return
   }
+
+
   return { firstUserTurnNormalMode, secondUserTurnNormalMode }
 }
