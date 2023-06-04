a8a8ef695c8a695183c0e57c15b826e0c8308eb4
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-06-04 12:15:00 +0900

    💄 update:top Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,24 +21,39 @@ export default function StartPage() {
   }
 
   return (
-    <div className=' flex h-screen w-screen items-center justify-center gap-x-10'>
-      <Link href={'/board'}>
-        <button onClick={handleEasyMode} className=' rounded-lg bg-blue-500 px-4 py-2 text-white'>
-          Easy
-        </button>
-      </Link>
+    <div className=' flex h-screen w-screen flex-col items-center justify-center gap-y-10 bg-gradient-to-b from-slate-200 to-slate-400'>
+      <div className='flex flex-col gap-y-2 text-center'>
+        <h1 className='font-serif text-4xl text-white opacity-80'>Memory Game</h1>
+        <h2 className='font-serif text-slate-200'>Train Your Brain with the Ultimate Memory Game Experience</h2>
+      </div>
+      <div className='flex gap-x-10 p-3'>
+        <Link href={'/board'}>
+          <button
+            onClick={handleEasyMode}
+            className='rounded-lg bg-gradient-to-r from-blue-400 to-blue-600 px-4 py-2 text-white hover:outline hover:outline-4 hover:outline-offset-4 hover:outline-blue-500'
+          >
+            Easy
+          </button>
+        </Link>
 
-      <Link href={'/board'}>
-        <button onClick={handleNormalMode} className=' rounded-lg bg-amber-500 px-4 py-2 text-white'>
-          Normal
-        </button>
-      </Link>
+        <Link href={'/board'}>
+          <button
+            onClick={handleNormalMode}
+            className='rounded-lg  bg-gradient-to-r from-amber-400 to-amber-600 px-4 py-2 text-white hover:outline hover:outline-4 hover:outline-offset-4 hover:outline-amber-500'
+          >
+            Normal
+          </button>
+        </Link>
 
-      <Link href={'/board'}>
-        <button onClick={handleHardMode} className=' rounded-lg bg-red-500 px-4 py-2 text-white'>
-          Hard
-        </button>
-      </Link>
+        <Link href={'/board'}>
+          <button
+            onClick={handleHardMode}
+            className='rounded-lg  bg-gradient-to-r from-red-400 to-red-600 px-4 py-2 text-white hover:outline hover:outline-4 hover:outline-offset-4 hover:outline-red-500'
+          >
+            Hard
+          </button>
+        </Link>
+      </div>
     </div>
   )
 }
