0e2fa290a8458c3ca72d579125e7f57d06adb3fe
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-06-04 13:06:57 +0900

    🩹 fix: for vercel deploy

--- src/common/hook/useAudio.tsx
+++ src/common/hook/useAudio.tsx
@@ -1,5 +1,13 @@
+import { useEffect, useState } from 'react'
+
 export const useAudio = () => {
-  const flipAudio = new Audio('/flip-audio.mp3')
-  const setCardAudio = new Audio('/user-get-card-audio.mp3')
+  const [flipAudio, setFlipAudio] = useState<HTMLAudioElement | null>(null)
+  const [setCardAudio, setSetCardAudio] = useState<HTMLAudioElement | null>(null)
+
+  useEffect(() => {
+    setFlipAudio(new Audio('/flip-audio.mp3'))
+    setSetCardAudio(new Audio('/user-get-card-audio.mp3'))
+  }, [])
+
   return { flipAudio, setCardAudio }
 }
