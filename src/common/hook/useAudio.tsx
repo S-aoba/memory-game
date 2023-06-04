3bce77f28498f0f48f07a112fcda2edd9d0e4538
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-06-04 12:53:12 +0900

    🐛 fix: audio path

--- src/common/hook/useAudio.tsx
+++ src/common/hook/useAudio.tsx
@@ -1,5 +1,5 @@
 export const useAudio = () => {
-  const flipAudio = new Audio('flip-audio.mp3')
-  const setCardAudio = new Audio('user-get-card-audio.mp3')
+  const flipAudio = new Audio('/flip-audio.mp3')
+  const setCardAudio = new Audio('/user-get-card-audio.mp3')
   return { flipAudio, setCardAudio }
 }
